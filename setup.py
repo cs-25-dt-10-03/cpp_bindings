752bbdd0f2b3fee64f989b150da6a078c852fd2a
Smedten <miksmedt@gmail.com>
2025-03-11 14:47:37 +0100

    fixed minor mistake

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ import pybind11
 ext_modules = [
     Extension(
         "flexoffer_logic",
-        ["bindings.cpp", "src/clusters.cpp", "src/flexoffer.cpp", "src/groups.cpp", "src/helpers.cpp", 
-         "src/DFO.cpp", "src/DFO_aggregation.cpp", "src/config.cpp"],
-=======
-        ["bindings.cpp", "src/clusters.cpp", "src/flexoffer.cpp", "src/groups.cpp", "src/helpers.cpp", "src/DFO.cpp", "src/DFO_aggregation.cpp", "src/DFO:disaggregation.cpp"],
->>>>>>> d40cb585b56c1a69d55c7bd654cc807274a9ec17
+        ["bindings.cpp", "src/clusters.cpp", "src/flexoffer.cpp", "src/groups.cpp", "src/helpers.cpp", "src/DFO.cpp", 
+         "src/DFO_aggregation.cpp", "src/DFO_disaggregation.cpp", "src/config.cpp"],
         include_dirs=[pybind11.get_include(), "../include"],  
         language="c++",
     ),
