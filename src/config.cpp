93dd30ceb278e75c1bda530ee50107fabb3ca043
Smedten <miksmedt@gmail.com>
2025-03-27 10:44:43 +0100

    added default 1 hour to config

--- src/config.cpp
+++ src/config.cpp
@@ -9,7 +9,7 @@ using json = nlohmann::json;
 int TIME_RESOLUTION = 3600; //fallback to 1 hour
 
 void loadConfig() {
-    const string path = "../master-thesis/config.json";
+    const string path = "../master_thesis/config.json";
     ifstream file(path);
     if (!file) {
         std::cerr << "Error: Could not open " << path << std::endl;
